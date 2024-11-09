12419f4b1d141d388595ca6c7830d635110a6453
robissoxtt <84225345+robissoxtt@users.noreply.github.com>
2024-11-09 01:39:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById('generate-btn').addEventListener('click', () => {
 // Função para resetar o avatar para o padrão
 document.getElementById('default-btn').addEventListener('click', () => {
     document.getElementById('avatar-image').src = 'images/default/default_pino.png';
-    clearLayers(); // Função auxiliar para remover os acessórios selecionados
+    clearLayers();
 });
 
 // Função para baixar o avatar
@@ -28,13 +28,11 @@ document.getElementById('download-btn').addEventListener('click', () => {
 
 // Função para definir um acessório no avatar
 function setAvatarLayer(category, image) {
-    // Verifica se a camada já existe, e remove para evitar duplicados
     const existingLayer = document.querySelector(`.layer[data-category="${category}"]`);
     if (existingLayer) {
         existingLayer.remove();
     }
 
-    // Cria uma nova camada com a imagem do acessório
     const layer = document.createElement('img');
     layer.src = `images/${category}/${image}`;
     layer.classList.add('layer');
@@ -42,7 +40,7 @@ function setAvatarLayer(category, image) {
     document.getElementById('avatar-display').appendChild(layer);
 }
 
-// Função auxiliar para limpar as camadas do avatar
+// Função para limpar as camadas do avatar
 function clearLayers() {
     const layers = document.querySelectorAll('.layer');
     layers.forEach(layer => layer.remove());
